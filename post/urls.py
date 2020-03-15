76932b19d1663e494d7a98f390c3b6e50f98f1d4
shahadat hossain <hridomheart@gmail.com>
2020-03-16 01:02:56 +0600

    view and create blog post

--- post/urls.py
+++ post/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', views.BlogListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', views.BlogDetails.as_view(), name='blog-details'),
+    path('post/new/', views.CreateBlog.as_view(), name='new-post'),
     path('about/', views.about, name='blog-about'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
 
 posts = Post.objects.all()
 
@@ -10,6 +12,26 @@ def home(request):
         'title': 'Shahadathb - Personal Blog'
     }
     return render(request, 'post/index.html', context)
+
+
+class BlogListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'post/index.html'
+    ordering = ['-posted_on']
+
+
+class BlogDetails(DetailView):
+    model = Post
+
+
+class CreateBlog(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
